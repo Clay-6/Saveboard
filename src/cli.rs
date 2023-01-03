bc38e10e3c522f5514b1009359621e0af58dd99b
Clay <claytonwambu@gmail.com>
2023-01-03 14:16:22 +0000

    Add proper help text

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,9 @@ use std::path::PathBuf;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
+#[clap(author, about, long_about=None)]
 pub struct Args {
-    #[clap(short, long)]
+    /// The file path to output your copied image as
+    #[clap(short, long, value_name = "PATH")]
     pub output: Option<PathBuf>,
 }
